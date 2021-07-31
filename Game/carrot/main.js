980c2cd7b369a34222f887b044d60792b028e689
maphnew <zcm3118@gmail.com>
2021-07-31 23:29:21 +0900

    🥕Carrot Game - Version1.0 Done

--- Game/carrot/main.js
+++ Game/carrot/main.js
@@ -8,6 +8,8 @@ const carrotSound = document.querySelector('#carrotSound')
 const bugSound = document.querySelector('#bugSound')
 const winSound = document.querySelector('#win')
 const alertSound = document.querySelector('#alert')
+const refresher = document.querySelector('.refresher')
+const refreshBtn = document.querySelector('.refresher__button')
 
 const createCatcher = (name) => {
     const catcher = document.createElement('img');
@@ -28,7 +30,9 @@ const randomPosition = () => {
     };
 }
 
-window.addEventListener('DOMContentLoaded', () => {
+const readyGame = () => {
+    field.innerHTML = '';
+    countNum.innerText = 0;
     for(let i = 0; i < 7; i++) {
         const bug = createCatcher('bug');
         field.appendChild(bug);
@@ -37,29 +41,48 @@ window.addEventListener('DOMContentLoaded', () => {
         const carrot = createCatcher('carrot');
         field.appendChild(carrot)
     }
-    
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    readyGame();
 })
 
+const result = (resultTxt) => {
+    refresher.style.display = 'unset';
+    const span = document.querySelector('.resultText');
+    span.innerText = resultTxt
+}
+
 field.addEventListener('click', e => {
+    if(playIcon.classList.contains('play')) {
+        return;
+    }
     if(e.target.classList.contains('carrot')) {
         carrotSound.play();
         countNum.innerText++;
         e.target.remove();
+        if(countNum.innerText == '10' && secTimer.innerText > 0 ) {
+            result('You Won!');
+            stopGame();
+        }
     }
     if(e.target.classList.contains('bug')) {
         bugSound.play();
+        result('You Lost!');
         stopGame();
     }
     
 })
 const startGame = () => {
     bgm.play();
-    playIcon.innerHTML = '<i class="fas fa-stop"></i>';
+    playIcon.classList.replace('play','stop')
+    playIcon.innerHTML = '<i class="fas fa-stop stop"></i>';
     secTimer.innerText = 10;
     
     countInterval = setInterval(() => {
         secTimer.innerText--
         if(secTimer.innerText == '0') {
+            result('You Lost!');
             stopGame();
         }
         secTimer.innerText = secTimer.innerText.padStart(2, 0);
@@ -67,15 +90,22 @@ const startGame = () => {
 }
 const stopGame = () => {
     bgm.pause();
-    playIcon.innerHTML = '<i class="fas fa-play"></i>';
-    secTimer.innerText = 10;
+    playIcon.classList.replace('stop','play')
+    playIcon.innerHTML = '<i class="fas fa-play play"></i>';
     clearInterval(countInterval);
 }
 let countInterval;
 playStopBtn.addEventListener('click', e => {
-    if(e.target.classList.contains('fa-play')) {
+    if(e.target.classList.contains('play')) {
         startGame();
+        playStopBtn.classList.replace('play','stop')
     }else{
         stopGame();
+        playStopBtn.classList.replace('stop','play')
     }
+})
+
+refreshBtn.addEventListener('click', () => {
+    refresher.style.display = 'none';
+    readyGame();
 })
