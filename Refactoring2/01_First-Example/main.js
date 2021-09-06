0bea4e6e626c1f3072599cd772c322946149e2be
maphnew <zcm3118@gmail.com>
2021-09-06 21:43:52 +0900

    🍔 function volumeCreditsFor

--- Refactoring2/01_First-Example/main.js
+++ Refactoring2/01_First-Example/main.js
@@ -12,20 +12,23 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
-
-        // 포인트를 적립한다
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 희극관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name} : ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name} : ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+}
+
+function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+        volumeCredits += Math.floor(aPerformance.audience / 5);
+    return volumeCredits;
 }
 
 function amountFor(aPerformance) {
