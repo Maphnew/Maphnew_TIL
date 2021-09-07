864b1276d6ce075e9cab3a833afd5ddfaa98f9e3
maphnew <zcm3118@gmail.com>
2021-09-07 21:47:57 +0900

    renderPlainText

--- Refactoring2/01_First-Example/statement.js
+++ Refactoring2/01_First-Example/statement.js
@@ -3,6 +3,30 @@ const invoices = require("./invoices.json");
 const createStatementData = require("./createStatement");
 const invoice = invoices[0];
 
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+    
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber/100);
+    }
+}
+
 function htmlStatement(invoice, plays) {
     return renderHtml(createStatementData(invoice, plays));
 }
@@ -28,8 +52,6 @@ function renderHtml(data) {
             minimumFractionDigits: 2
         }).format(aNumber/100);
     }
-    
-    
 }
 
-console.log(htmlStatement(invoice, plays));
+console.log(statement(invoice, plays));
