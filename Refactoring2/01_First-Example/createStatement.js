e3e7cb00927461443a215aceba86ab2a5b44fe1b
maphnew <zcm3118@gmail.com>
2021-09-07 21:32:07 +0900

    🎆 modules!

--- Refactoring2/01_First-Example/createStatement.js
+++ Refactoring2/01_First-Example/createStatement.js
@@ -1,18 +1,10 @@
-const plays = require("./plays.json");
-const invoices = require("./invoices.json");
-const invoice = invoices[0];
-
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
-}
-
 function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
     
     function totalAmount(data) {
         return data.performances.reduce((total, p) => total + p.amount, 0);
@@ -65,26 +57,4 @@ function createStatementData(invoice, plays) {
     }
 }
 
-function renderPlainText(data, plays) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-    return result;
-    
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber/100);
-    }
-    
-    
-}
-
-console.log(statement(invoice, plays));
+module.exports = createStatementData;
---
+++ Refactoring2/01_First-Example/statement.js
@@ -0,0 +1,32 @@
+const plays = require("./plays.json");
+const invoices = require("./invoices.json");
+const createStatementData = require("./createStatement");
+const invoice = invoices[0];
+
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+    
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber/100);
+    }
+    
+    
+}
+
+console.log(statement(invoice, plays));
